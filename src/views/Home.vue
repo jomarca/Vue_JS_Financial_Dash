0fdd260bcd47aebff496f0beedfc51233d5feb45
CodingandFun89 <jomarca23@gmail.com>
2020-03-23 22:40:24 +0100

    second

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <StockInfo />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import StockInfo from '@/components/StockInfo.vue'
+
+export default {
+  name: 'home',
+  components: {
+    StockInfo
+  }
+}
+</script>
